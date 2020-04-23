046507cb2bb9ce8b038bccbd2133de820b2170ff
Julio Benavente <32945829+julio-benavente@users.noreply.github.com>
2020-04-22 22:28:12 -0500

    Updates

---
+++ precache-manifest.875c45849f5c892e9a9e816f9bb3d929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259338e39efb6b1bc15e29bef4f9a240",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "36b2355d533cd986a01d",
+    "url": "/movies/static/css/main.5aabee46.chunk.css"
+  },
+  {
+    "revision": "0fcba90542c7cca35560",
+    "url": "/movies/static/js/2.d44a1b2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies/static/js/2.d44a1b2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b2355d533cd986a01d",
+    "url": "/movies/static/js/main.bf08cf19.chunk.js"
+  },
+  {
+    "revision": "9d6183adf5ed941c5247",
+    "url": "/movies/static/js/runtime-main.83557c5e.js"
+  }
+]);
